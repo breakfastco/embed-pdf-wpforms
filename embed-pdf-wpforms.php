cadcc7124baa9806abccba55798510e659dd6442
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-10-10 19:42:39 -0400

    Avoid errors if more than one copy of the plugin are activated at the same time.

--- embed-pdf-wpforms.php
+++ embed-pdf-wpforms.php
@@ -22,31 +22,33 @@ if ( ! defined( 'EMBED_PDF_WPFORMS_VERSION' ) ) {
 	define( 'EMBED_PDF_WPFORMS_VERSION', '1.0.0' );
 }
 
-add_action( 'wpforms_loaded', 'embed_pdf_wpforms_init', 5 );
-/**
- * Loads the plugin files and features.
- *
- * @return void
- */
-function embed_pdf_wpforms_init() {
-	if ( ! class_exists( 'WPForms_Field' ) ) {
-		return;
+if ( ! function_exists( 'embed_pdf_wpforms_init' ) ) {
+	add_action( 'wpforms_loaded', 'embed_pdf_wpforms_init', 5 );
+	/**
+	 * Loads the plugin files and features.
+	 *
+	 * @return void
+	 */
+	function embed_pdf_wpforms_init() {
+		if ( ! class_exists( 'WPForms_Field' ) ) {
+			return;
+		}
+		require_once dirname( EMBED_PDF_WPFORMS_PATH ) . '/includes/class-wpforms-field-pdf-viewer.php';
+		new WPForms_Field_PDF_Viewer();
+
+		// Add our field type to the list of allowed fields in wpforms_get_form_fields().
+		add_filter( 'wpforms_get_form_fields_allowed', 'embed_pdf_wpforms_field_types' );
 	}
-	require_once dirname( EMBED_PDF_WPFORMS_PATH ) . '/includes/class-wpforms-field-pdf-viewer.php';
-	new WPForms_Field_PDF_Viewer();
 
-	// Add our field type to the list of allowed fields in wpforms_get_form_fields().
-	add_filter( 'wpforms_get_form_fields_allowed', 'embed_pdf_wpforms_field_types' );
-}
-
-/**
- * Adds our field type to the list of field types WPForms recognizes as
- * valid.
- *
- * @param  array $allowed_form_fields Allow list of field types.
- * @return array
- */
-function embed_pdf_wpforms_field_types( $allowed_form_fields ) {
-	$allowed_form_fields[] = 'pdf_viewer';
-	return $allowed_form_fields;
+	/**
+	 * Adds our field type to the list of field types WPForms recognizes as
+	 * valid.
+	 *
+	 * @param  array $allowed_form_fields Allow list of field types.
+	 * @return array
+	 */
+	function embed_pdf_wpforms_field_types( $allowed_form_fields ) {
+		$allowed_form_fields[] = 'pdf_viewer';
+		return $allowed_form_fields;
+	}
 }
